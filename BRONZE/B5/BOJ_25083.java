7c7103b735dbdb0bf01ffae7fbf71b443ca8e7a5
Jaecheol Jeong <cheolup1111@gmail.com>
2022-05-02 23:34:51 +0900

    BOJ_25083_새싹

---
+++ BRONZE/B5/BOJ_25083.java
@@ -0,0 +1,15 @@
+// BOJ_25083_새싹
+
+public class BOJ_25083 {
+    public static void main(String[] args){
+        /* ==========input========== */
+        /* ==========sol========== */
+        /* ==========output========== */
+        System.out.println("         ,r'\"7");
+        System.out.println("r`-_   ,'  ,/");
+        System.out.println(" \\. \". L_r'");
+        System.out.println("   `~\\/");
+        System.out.println("      |");
+        System.out.println("      |");
+    }
+}
